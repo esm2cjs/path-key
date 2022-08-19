36405c10c8703f685c9509bdbdb7687a3c3dd09e
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-08-19 22:06:40 +0200

    ci: sync workflows and scripts between repos

--- .github/make_hybrid.sh
+++ .github/make_hybrid.sh
@@ -34,6 +34,13 @@ PJSON=$(cat package.json | jq --tab '
 ')
 echo "$PJSON" > package.json
 
+# Update package.json -> version if upstream forgot to update it
+if [[ ! -z "${TAG}" ]] ; then
+	VERSION=$(echo "${TAG/v/}")
+	PJSON=$(cat package.json | jq --tab --arg VERSION "$VERSION" '.version = $VERSION')
+	echo "$PJSON" > package.json
+fi
+
 npm i -D @alcalzone/esm2cjs
 npm run to-cjs
 npm uninstall -D @alcalzone/esm2cjs
